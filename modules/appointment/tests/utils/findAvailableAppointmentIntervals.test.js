02069a4dc6c4d8622af5d4c4cec3ea075f8fdcd6
Trey Martin <trey@M2.local>
2024-08-27 02:52:09 -0400

    chore: clean up stale import

--- modules/appointment/tests/utils/findAvailableAppointmentIntervals.test.js
+++ modules/appointment/tests/utils/findAvailableAppointmentIntervals.test.js
@@ -1,4 +1,4 @@
-import { expect, test, describe, it } from 'vitest';
+import { expect, describe, it } from 'vitest';
 import { findAvailableAppointmentIntervals } from '../../utils.js';
 
 const f = {
