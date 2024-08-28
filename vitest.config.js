2361e769a5bbd282606b6926e871999225ba21d4
Trey Martin <trey@M2.local>
2024-08-28 03:07:34 -0400

    fix: remove stale comments

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,7 @@
-// vitest.config.js
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // You can specify the reporter here
-    reporters: ['default', 'verbose'], // or use 'dot', 'json', etc.
+    reporters: ['default', 'verbose'],
   },
 });
